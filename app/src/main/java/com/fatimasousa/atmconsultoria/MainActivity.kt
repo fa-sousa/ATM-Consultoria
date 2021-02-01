1524b68d58b3ac4c15cc063a714071bfd3221662
Fatima Ferreira de Sousa <fatimaferreiradesousa@8849-nb.home>
2021-02-01 17:07:29 -0300

    simula fluxo de vida da activity

--- app/src/main/java/com/fatimasousa/atmconsultoria/MainActivity.kt
+++ app/src/main/java/com/fatimasousa/atmconsultoria/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fatimasousa.atmconsultoria
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(applicationContext,"onCreate",Toast.LENGTH_LONG).show()
 
         txt_clientes.setOnClickListener {
             val intent = Intent(this, DetalheClientesActivity::class.java)
@@ -31,4 +34,35 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(applicationContext,"onStart",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(applicationContext,"onResume",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(applicationContext,"onPause",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(applicationContext,"onStop",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(applicationContext,"onRestart",Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(applicationContext,"onDestroy",Toast.LENGTH_LONG).show()
+    }
+
 }
